203ea90d98a1ef68a673cf442790ba90e4b2bd9e
Rajkumar29Dec <42741001+Rajkumar29Dec@users.noreply.github.com>
2020-02-20 11:33:37 +0530

    Add files via upload

--- src/app.js
+++ src/app.js
@@ -21,12 +21,21 @@ const PLAYERS = [
     "Slingo"
 ];
 
+var i=0;
 // initialize players with image and strength
 const initPlayers = (players) => {
     let detailedPlayers = '';
 
     // Instead of forloop use Map method
     // Code here
+    detailedPlayers=players.map(function(){
+       return {
+        name:players[i],
+        image:"images/super-"+(i+1)+".png",
+        strength : getRandomStrength(),
+        type: "hero"
+        };
+    });
 
     return detailedPlayers;
 }
@@ -43,6 +52,11 @@ const buildPlayers = (players, type) => {
     // Instead of using for loop
     // Use chaining of Array methods - filter, map and join
     // Type your code here
+    fragment=`<div class="player">
+    <img src="${players[i].image}" alt=" ">
+    <div class="name">${players[i].name}</div>
+    <div class="strength">${players[i].strength}</div>
+</div>`
 
     return fragment;
 }
